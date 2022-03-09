c923b8470e58c162135675c9360279e079106a44
Spyder-Monkey <tbender@ptd.net>
2022-03-08 21:38:29 -0500

    gets correct answer, but exceeds time

--- government_help/government.py
+++ government_help/government.py
@@ -1,32 +1,47 @@
-
-# Take the smallest difference between A and B
-def maximal_diff(lst):
-    if all(element == lst[0] for element in lst):
-        return lst
-
-    packs = lst.copy()
-    
-    A = packs.pop(packs.index(min(packs)))
-    B = packs.pop(packs.index(max(packs)))
-
-    print(A - B)
-    print(packs)
+from itertools import permutations
+import numpy as np
 
 
-def main():
-    packages = []
-
+def get_input():
+    lst = []
     while True:
         N = int(input())
         if N == 0: break
-        package = list(map(int, input().split(' ')))
-        packages.append(package)
 
+        temp = list(map(int, input().split(' ')))
+        lst.append(temp)
+    return lst
 
-    for i in packages:
-        maximal_diff(i)
+def get_diff(lst):
+    max_diff = []
+    A, B = 0, 0
 
-    print(packages)
+    for i in range(len(lst)):
+        temp = []
+        for x in range(len(lst[0])):
+            if x % 2 == 0: A += lst[i][x]
+            else: B += lst[i][x]
+
+            temp.append(abs(A-B))
+        max_diff.append(max(temp))
+        A, B = 0, 0
+
+    return max_diff.index(min(max_diff))
+
+def main():
+    packages = get_input()
+    maximal_diff = []
+
+    for i in range(len(packages)):
+        temp = packages[i].copy()
+        perms = [a for a in permutations(temp, len(temp))]
+        maximal_diff.append(perms[get_diff(perms)])
+
+    for i in range(len(maximal_diff)):
+        str = ''
+        for j in range(len(maximal_diff[i])):
+            str+= f"{maximal_diff[i][j]}-A " if j%2==0 else f"{maximal_diff[i][j]}-B "
+        print(str)
 
 if __name__=="__main__":
-    main()
+    main()  
